10d47ee33171d68b350b682b11c01f2a66799199
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-25 18:01:45 +0900

    chore: add tsdx

--- test/cloudfront-s3.test.ts
+++ test/cloudfront-s3.test.ts
@@ -1,4 +1,8 @@
-import { expect as expectCDK, haveResource, SynthUtils, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {
+  expect as expectCDK,
+  haveResource,
+  SynthUtils,
+} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CloudfrontS3 from '../lib/index';
 import { Bucket } from '@aws-cdk/aws-s3';
@@ -6,63 +10,63 @@ import { Bucket } from '@aws-cdk/aws-s3';
 describe('Resource creation', () => {
   describe('default', () => {
     const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
-    new CloudfrontS3.CloudfrontS3(stack, 'MyTestConstruct', {
-      name: 'example'
-    });
-    it.each([
-      "AWS::IAM::Role",
-      "AWS::Lambda::Function",
-      "AWS::Lambda::Version",
-      "AWS::S3::Bucket",
-      "AWS::S3::BucketPolicy",
-      "AWS::CloudFront::CloudFrontOriginAccessIdentity",
-      "AWS::CloudFront::Distribution"
-    ])("Should create a %p resourec", (type) => {
-      expectCDK(stack).to(haveResource(type))
-    })
-  })
-  describe('import s3 bucket', () => {
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
+    const stack = new cdk.Stack(app, 'TestStack');
     new CloudfrontS3.CloudfrontS3(stack, 'MyTestConstruct', {
       name: 'example',
-      s3Bucket: Bucket.fromBucketName(stack, 'Dummy', 'dummy')
     });
     it.each([
-      "AWS::IAM::Role",
-      "AWS::Lambda::Function",
-      "AWS::Lambda::Version",
-      "AWS::CloudFront::CloudFrontOriginAccessIdentity",
-      "AWS::CloudFront::Distribution"
-    ])("Should create a %p resourec", (type) => {
-      expectCDK(stack).to(haveResource(type))
-    })
+      'AWS::IAM::Role',
+      'AWS::Lambda::Function',
+      'AWS::Lambda::Version',
+      'AWS::S3::Bucket',
+      'AWS::S3::BucketPolicy',
+      'AWS::CloudFront::CloudFrontOriginAccessIdentity',
+      'AWS::CloudFront::Distribution',
+    ])('Should create a %p resourec', type => {
+      expectCDK(stack).to(haveResource(type));
+    });
+  });
+  describe('import s3 bucket', () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'TestStack');
+    new CloudfrontS3.CloudfrontS3(stack, 'MyTestConstruct', {
+      name: 'example',
+      s3Bucket: Bucket.fromBucketName(stack, 'Dummy', 'dummy'),
+    });
     it.each([
-      "AWS::S3::Bucket",
-      "AWS::S3::BucketPolicy",
-    ])("Should not create a %p resourec", (type) => {
-      expectCDK(stack).notTo(haveResource(type))
-    })
-  })
-})
+      'AWS::IAM::Role',
+      'AWS::Lambda::Function',
+      'AWS::Lambda::Version',
+      'AWS::CloudFront::CloudFrontOriginAccessIdentity',
+      'AWS::CloudFront::Distribution',
+    ])('Should create a %p resourec', type => {
+      expectCDK(stack).to(haveResource(type));
+    });
+    it.each(['AWS::S3::Bucket', 'AWS::S3::BucketPolicy'])(
+      'Should not create a %p resourec',
+      type => {
+        expectCDK(stack).notTo(haveResource(type));
+      }
+    );
+  });
+});
 describe('stack snapshot', () => {
   it.each([
     {},
-    {s3BucketName: 'dummy'},
-    {domains: ['example.com'], acmCertificationARN: 'dummyarn'}
-  ])("should match snapthos", (conf) => {
+    { s3BucketName: 'dummy' },
+    { domains: ['example.com'], acmCertificationARN: 'dummyarn' },
+  ])('should match snapthos', conf => {
     const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
+    const stack = new cdk.Stack(app, 'TestStack');
     if (conf.s3BucketName) {
       // @ts-ignore
-      conf.s3Bucket = Bucket.fromBucketName(stack, 'Dummy', conf.s3BucketName)
-      delete conf.s3BucketName
+      conf.s3Bucket = Bucket.fromBucketName(stack, 'Dummy', conf.s3BucketName);
+      delete conf.s3BucketName;
     }
     new CloudfrontS3.CloudfrontS3(stack, 'MyTestConstruct', {
       name: 'example',
-      ...conf as CloudfrontS3.CloudfrontS3Props,
+      ...(conf as CloudfrontS3.CloudfrontS3Props),
     });
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
-  })
-})
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+});
