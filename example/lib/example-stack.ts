302d6b5930ac67792db28d90e0ce5b8960352fa0
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-29 23:32:41 +0900

    feat: can add edge function (ignore origin-request)

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import {createHash} from 'crypto'
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import {
   CloudfrontS3, CloudfrontS3Props
 } from '../../lib'
@@ -13,7 +18,26 @@ export class ExampleStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const params: CloudfrontS3Props = {...options}
+    const EDGE_URL_HANDLER_PATH = join(__dirname, '/lambda/originResponse.ts')
+    const func = new NodejsFunction(this, 'OriginRequestHandler', {
+      handler: 'handler',
+      runtime: Runtime.NODEJS_12_X,
+      functionName: `ExampleEdgeFunction`,
+      entry: EDGE_URL_HANDLER_PATH,
+    })
+    const params: CloudfrontS3Props = {
+      ...options,
+      edgeFunctions: {
+        "origin-response": func.addVersion(
+          createHash('sha256').update(
+            readFileSync(
+              EDGE_URL_HANDLER_PATH,
+              'utf-8'
+            )
+          ).digest('hex')
+        )
+      }
+    }
     if (options.s3BucketArn) {
       params.s3Bucket = Bucket.fromBucketArn(this, 'ImportBucket', options.s3BucketArn)
     } else if (options.s3BucketName) {
